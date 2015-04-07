d3d2684bf0885965fa12bfe72482715faf33aa89
Dimitris Klisiaris <dklisiaris@gmail.com>
2015-04-08 01:45:53 +0300

    Add mail functionality through mandrill.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,24 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+
+  # change to true to allow email to be sent during development
+  config.action_mailer.perform_deliveries = true
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    :domain => "bookshark.eu",
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587,
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_API_KEY"]
+  }  
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
